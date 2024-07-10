d94f8859e99fb1bd2a354d8945b5c9f93feb70a3
Zenkearth <Peeraphatnaowasaisee1@gmail.com>
2024-07-10 13:05:01 +0700

    optimized partially of code

--- HW1/script.js
+++
@@ -1,9 +0,0 @@
-var loadFile = function(event) {
-    let image = document.getElementById('output');
-    let loadText = document.getElementById('upload_area');
-    image.src = URL.createObjectURL(event.target.files[0]);
-    loadText.textContent='';
-    image.onload = function() {
-        URL.revokeObjectURL(image.src)
-    }
-};
